c590037dc1d638d1726c0a27f4a7f38674cc9741
Blank10763 <matthewmita01@gmail.com>
2025-10-20 17:27:09 -0400

    Refactor AES encryption and decryption functions

--- Lab-2/new_aes.py
+++ Lab-2/new_aes.py
@@ -5,36 +5,31 @@ BLOCK = 16
 
 def pad(b):
     """Pad data so length is a multiple of 16 bytes."""
-    pad_len = BLOCK - (len(b) % BLOCK)
-    return b + bytes([pad_len] * pad_len)
+    i = BLOCK - (len(b) % BLOCK)
+    return b + bytes([i] * i)
 
 def unpad(b):
     """Remove padding added by pad()."""
-    pad_len = b[-1]
-    return b[:-pad_len]
+    len = b[-1]
+    return b[:-len]
 
 def encrypt_ecb_blocks(pt, key):
     """Encrypt data with AES in ECB mode."""
-    cipher = AES.new(key, AES.MODE_ECB)
-    pt_padded = pad(pt)
-    return cipher.encrypt(pt_padded)
+    data = AES.new(key, AES.MODE_ECB)
+    return data.encrypt(pad(pt))
 
 def decrypt_ecb_blocks(ct, key):
     """Decrypt data with AES in ECB mode."""
-    cipher = AES.new(key, AES.MODE_ECB)
-    pt_padded = cipher.decrypt(ct)
-    return unpad(pt_padded)
+    data = AES.new(key, AES.MODE_ECB)
+    return unpad(data.decrypt(ct))
 
 def encrypt_cbc_blocks(pt, key):
     """Encrypt data with AES in CBC mode, return (iv, ciphertext)."""
     iv = os.urandom(BLOCK)
-    cipher = AES.new(key, AES.MODE_CBC, iv)
-    pt_padded = pad(pt)
-    ct = cipher.encrypt(pt_padded)
-    return iv, ct
+    data = AES.new(key, AES.MODE_CBC, iv)
+    return iv, data.encrypt(pad(pt))
 
 def decrypt_cbc_blocks(iv, ct, key):
     """Decrypt data with AES in CBC mode."""
-    cipher = AES.new(key, AES.MODE_CBC, iv)
-    pt_padded = cipher.decrypt(ct)
-    return unpad(pt_padded)
+    data = AES.new(key, AES.MODE_CBC, iv)
+    return unpad(data.decrypt(ct))
