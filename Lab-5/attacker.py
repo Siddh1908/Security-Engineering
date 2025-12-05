e15e3866a318ff16d2501e508fe7bd14963da7e6
Siddh Bhagat <siddhbhagat@umass.edu>
2025-12-05 16:15:55 -0500

    fixing orginization

---
+++ Lab-5/attacker.py
@@ -0,0 +1,20 @@
+import socket, time, statistics, string
+SOCK_PATH = "/tmp/passwordchecker.sock"
+
+"""TODO: This is the max amount if recommended trials.
+In your lab report, talk about what will happen
+if the trial count goes lower? What do you see?"""
+TRIALS = 120
+
+"""TODO: Fill in the alphabet"""
+ALPHABET = "" 
+
+
+def measure():
+    """TODO"""
+
+def recover(max_len=7):
+    """TODO"""
+
+if __name__=="__main__":
+    recover()
